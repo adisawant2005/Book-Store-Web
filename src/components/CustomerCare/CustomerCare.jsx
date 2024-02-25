8561a7d5341db9ebe614ce5ff83fe485ead355ea
Aditya Sawant <adisawant454@gmail.com>
2024-02-25 09:23:14 +0530

    Updated Customer Care

--- src/components/CustomerCare/CustomerCare.jsx
+++ src/components/CustomerCare/CustomerCare.jsx
@@ -1,5 +1,9 @@
 const CustomerCare = () => {
-  return;
+  return (
+    <main className="border-8 bg-[url(CutomerCare.jpg)] w-screen h-[150vh] bg-contain bg-no-repeat bg-center">
+      <h1>Customer Care</h1>
+    </main>
+  );
 };
 
 export default CustomerCare;
